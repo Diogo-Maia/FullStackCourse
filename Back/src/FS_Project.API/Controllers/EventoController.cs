32e363610326b00b2954b80de3cd638a522c181d
Diogo-Maia <Diogo_Maia99@hotmail.com>
2022-03-25 01:32:18 +0000

    Section2 NetCore5

---
+++ Back/src/FS_Project.API/Controllers/EventoController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FS_Project.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FS_Project.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _eventos = new Evento[]
+        {
+            new Evento()
+                {
+                    EventoId = 1,
+                    Tema = "Full Stack Udemy",
+                    Local = "Lisboa",
+                    Lote = "6A",
+                    NumPessoas = 2,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImagemURL = "foto.png"
+                },
+                new Evento()
+                {
+                    EventoId = 2,
+                    Tema = "Full Stack Udemy 2",
+                    Local = "Lisboa",
+                    Lote = "6B",
+                    NumPessoas = 1,
+                    DataEvento = DateTime.Now.AddDays(3).ToString(),
+                    ImagemURL = "imagem.png"
+                }
+        };
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _eventos;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetbyID(int id)
+        {
+            return _eventos.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo Put | ID - {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo Delete | ID - {id}";
+        }
+    }
+}
