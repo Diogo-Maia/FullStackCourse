ad1c001cd580edbabbbc477a7d8ab4d2df2e21fd
diogo-maia <Diogo_Maia99@hotmail.com>
2022-03-28 00:46:26 +0100

    Section5 AngularPt2

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltred: any = [];
+  public widthImg: number = 150;
+  public marginImg: number = 2;
+  public showImg: boolean = true;
+
+  private _filterList: string = "";
+
+  public get filterList(){
+    return this._filterList;
+  }
+
+  public set filterList(value: string){
+    this._filterList = value;
+    this.eventosFiltred = this.filterList ? this.filterEvents(this.filterList): this.eventos
+  }
+
+  filterEvents(filterValue: string): any{
+    filterValue = filterValue.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filterValue) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterValue) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +39,16 @@ export class EventosComponent implements OnInit {
     this.GetEventos();
   }
 
+  showImage(){
+    this.showImg = !this.showImg;
+  }
+
   public GetEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltred = response;
+      },
       error => console.log(error)
     );
 
